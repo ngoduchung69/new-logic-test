bedbb58ee98d2e11798e20a5fac7c4aa2cda7fcc
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-09-07 15:58:22 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./views/Home";
 import ConsentsPage from "./views/Consents";
 import HomePageLayout from "./Layouts/HomePageLayout";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [publicKey, setPublicKey] = useState('')
+  useEffect(() => {
+    axios.get('http://localhost:3000/stripe/config').then(({data}) => setPublicKey(data?.stripePublicKey))
+  }, [])
   return (
     <Router>
       <HomePageLayout>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/" element={publicKey ? <Elements stripe={loadStripe(publicKey)}><HomePage /></Elements> : <></>} />
           <Route path="/consents" element={<ConsentsPage />} />
         </Routes>
       </HomePageLayout>
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,28 +1,116 @@
-import {
-  Container,
-  Title,
-  Wrapper,
-} from "../../assets/styles/home/home-styles";
-import { useState } from "react";
-import Form from "./Form";
-import ReceiveSpeech from "./ReceiveSpeech";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect } from "react";
 
 const HomePage = () => {
-  const [screenStatus, setScreenStatus] = useState(false);
-  const changeScreenStatus = () => {
-    setScreenStatus(!screenStatus)
+  const element = useElements();
+  const stripe = useStripe();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: element.getElement(CardElement),
+    });
+
+    const data = await fetch(
+      "http://localhost:3000/stripe/pay",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          paymentMethodType: "card",
+          currency: "usd",
+          amount: 90000,
+        }),
+      }
+    ).then((result) => result.json());
+    console.log({ data });
+
+    if (!error) {
+      const { id } = paymentMethod;
+      const {paymentIntent} = await stripe.confirmCardPayment(data.client_secret, {
+        payment_method: id,
+        save_payment_method: true,
+      });
+      console.log({ paymentIntent });
+      if(paymentIntent) {
+        const createReuseRecord = await fetch(
+          "http://localhost:3000/stripe/create-reuse-record",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              user_id: 1,
+              payment_method_id: paymentIntent.payment_method,
+              customer_id: data.customer
+            }),
+          }
+        ).then((result) => result.json());
+
+        console.log({createReuseRecord})
+      }
+    }
+
+
+    // const data = await fetch('http://localhost:3000/payments/create-checkout-session', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     paymentMethodType: 'card',
+    //     currency: 'usd',
+    //   })
+    // }).then(result => result.json())
+
+    // console.log({data})
+
+    // const data2 = await stripe.confirmCardPayment(data.client_secret, {
+    //   payment_method: {
+    //     card: element.getElement(CardElement)
+    //   }
+    // })
+
+    // console.log({paymentIntent: data2})
+  };
+
+  const reusePayment = async () => {
+    const reuse = await fetch(
+      "http://localhost:3000/stripe/pay",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: 1,
+          paymentMethodType: "card",
+          currency: "usd",
+          amount: 70000,
+        }),
+      }
+    ).then((result) => result.json());
+
+    console.log({reuse})
   }
   return (
-    <Wrapper>
-      <Container>
-        <Title>Consents Form</Title>
-        {!screenStatus ? (
-          <Form changeScreenStatus={changeScreenStatus} />
-        ) : (
-          <ReceiveSpeech />
-        )}
-      </Container>
-    </Wrapper>
+    <div>
+      <form
+        action="/create-checkout-session"
+        style={{ width: "1000px" }}
+        onSubmit={submit}
+        id="payment-form"
+      >
+        <CardElement />
+        <button>Pay for the first Time</button>
+      </form>
+
+      <button onClick={reusePayment}>reuse payment for the second time</button>
+    </div>
   );
 };
 
